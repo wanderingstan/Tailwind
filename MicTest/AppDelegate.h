b673d719c241da6ab2b8532cbbe74348c0a83d50
Stan James <stan@sphero.com>
2017-03-27 16:37:56 -0600

    Initial Commit

---
+++ MicTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MicTest
+//
+//  Created by Stan on 3/27/17.
+//  Copyright © 2017 Stan James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MicTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MicTest
+//
+//  Created by Stan on 3/27/17.
+//  Copyright © 2017 Stan James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
